8e038a808dba9917d77059fc657bfa9d730827f8
Thurneer <florian00m14@gmail.com>
2023-11-14 01:51:41 +0100

    Update piece.js

--- js/piece.js
+++ js/piece.js
@@ -1,5 +1,6 @@
 import SVGLoader from "./svgLoader.js";
 import { isInCheck, findKing } from "./board.js";
+
 class Piece {
   constructor(type, color) {
     this.color = color;
@@ -257,7 +258,7 @@ class Piece {
 
   loadSvg() {
     // SVG betöltése
-    const svgPath = `${window.location.origin}/assets/${this.color}-${this.type}.svg`;
+    const svgPath = `${window.location.origin}/${window.location.pathname}/assets/${this.color}-${this.type}.svg`;
     const loader = new SVGLoader(svgPath);
     return loader.loadSVG();
   }
